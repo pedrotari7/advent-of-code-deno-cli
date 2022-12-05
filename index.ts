4797b11ebbaa99f1a7dc4d42ff1574acea1fab1c
Pedro Alvito <pedrotari7@gmail.com>
2022-12-05 18:55:04 +0100

    adjust template for single file

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
-import InputLoop from "https://deno.land/x/input@2.0.3/index.ts";
+import { input, confirm } from "https://deno.land/x/inquirer/mod.ts";
 
 // @deno-types="https://deno.land/x/chalk_deno@v4.1.1-deno/index.d.ts"
 import chalk from "https://deno.land/x/chalk_deno@v4.1.1-deno/source/index.js";
@@ -44,31 +44,42 @@ const getProblemInput = async (event: number, n: number): Promise<void> => {
 
   const text = template(n);
 
-  for (const part of ["A", "B"]) {
-    const filePath = `ts/${event}/${n}${part}.ts`;
-    if (!(await exists(filePath))) {
-      await Deno.writeTextFile(filePath, text, { create: true });
-      console.log(chalk.green(`Typescript file ${filePath} generated!`));
-    } else {
-      console.log(chalk.red(`Typescript file ${filePath} already exits!`));
-    }
+  const codeFilePath = `ts/${event}/${n}.ts`;
+  if (!(await exists(codeFilePath))) {
+    await Deno.writeTextFile(codeFilePath, text, { create: true });
+    console.log(chalk.green(`Typescript file ${codeFilePath} generated!`));
+  } else {
+    console.log(chalk.red(`Typescript file ${codeFilePath} already exits!`));
   }
 };
 
 const args = parse(Deno.args);
 
-const input = new InputLoop();
-
 let session = localStorage.getItem("session");
 if (!session || args.s) {
   console.log("Session cookie not available");
-  session = await input.question("Enter the session cookie:");
-  localStorage.setItem("session", session);
+  session = await input({ message: "Enter the session cookie:", default: "" });
+  if (session) {
+    localStorage.setItem("session", session);
+  }
+}
+
+let aocFolder = localStorage.getItem("folder");
+if (!aocFolder || args.folder) {
+  console.log();
+
+  const isCurrentFolder = await confirm({ message: `Is the current folder '${Deno.cwd()}' correct?` });
+
+  console.log("isCurrentFolder", isCurrentFolder);
+  if (isCurrentFolder) {
+    aocFolder = Deno.cwd();
+    localStorage.setItem("folder", aocFolder);
+  }
 }
 
 const event = args.y ?? 2022;
 const day = args.d;
 
-if (args.i) {
+if (args.i && aocFolder && session && day) {
   await getProblemInput(event, day);
 }
--- template.ts
+++ template.ts
@@ -1,10 +1,16 @@
 export const template = (day: number) =>
-	`import { timer, getSplittedDataFromFile } from '../utilities.ts';
+  `import { timer, getSplittedDataFromFile } from '../utilities.ts';
 
 timer.start();
 
 const data = getSplittedDataFromFile(${day});
-console.log(data);
+
+const p1 = data.length;
+
+const p2 = data.length;
+
+console.log('p1', p1);
+console.log('p2', p2);
 
 timer.stop();
 `;
