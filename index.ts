7d54d476bd8037d29cf138a43d0e47ba7ce706f5
Pedro Alvito <pedrotari7@gmail.com>
2022-12-08 20:00:31 +0100

    leaderboard info cli

--- index.ts
+++ index.ts
@@ -1,10 +1,38 @@
 import { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
 import { input, confirm } from "https://deno.land/x/inquirer@v0.0.4/mod.ts";
 import { exec, OutputMode } from "https://deno.land/x/exec@0.0.5/mod.ts";
+import { writeAllSync } from "https://deno.land/std@0.167.0/streams/write_all.ts";
 
 // @deno-types="https://deno.land/x/chalk_deno@v4.1.1-deno/index.d.ts"
 import chalk from "https://deno.land/x/chalk_deno@v4.1.1-deno/source/index.js";
 import { template } from "./template.ts";
+
+interface Leaderboard {
+  members: Record<string, User>;
+  owner_id: string;
+  event: string;
+}
+
+interface User {
+  name: string;
+  last_star_ts: number;
+  stars: number;
+  global_score: number;
+  local_score: number;
+  id: number;
+  completion_day_level:
+    | Record<number, never>
+    | Record<
+        number,
+        {
+          1: { get_star_ts: number; star_index: number };
+          2?: { get_star_ts: number; star_index: number };
+        }
+      >;
+}
+
+const write = (str: string | number | unknown, end = " ") =>
+  writeAllSync(Deno.stdout, new TextEncoder().encode(`${str}` + end));
 
 const exists = async (filename: string): Promise<boolean> => {
   try {
@@ -19,8 +47,45 @@ const exists = async (filename: string): Promise<boolean> => {
   }
 };
 
+const getLeaderboardInfo = async (event: number, id: number) => {
+  const response = await fetch(`https://adventofcode.com/${event}/leaderboard/private/view/${id}.json`, {
+    headers: { Cookie: `session=${session}` },
+  });
+
+  if (response.ok) {
+    const info: Leaderboard = await response.json();
+
+    const users = Object.values(info.members)
+      .filter((u) => u.local_score)
+      .sort((a, b) => a.local_score - b.local_score);
+
+    for (const [index, user] of users.entries()) {
+      write(`${users.length - index}`.padStart(3, " "));
+      write(`${user.local_score}`.padStart(6, " "));
+
+      for (let d = 1; d < 25; d++) {
+        if (user.completion_day_level[d]) {
+          if (user.completion_day_level[d][2]) {
+            write(chalk.yellow("*"));
+          } else {
+            write(chalk.blue("*"));
+          }
+        } else {
+          write(" ");
+        }
+      }
+
+      const name = user.name ?? `anon #${user.id}`;
+      const userName = user.global_score ? `${name} (${user.global_score})` : user.name;
+
+      write(userName);
+      write("\n", "");
+    }
+  }
+};
+
 const getProblemInput = async (event: number, n: number): Promise<void> => {
-  const filePath = `ts/${event}/${n}.in`;
+  const filePath = `${aocFolder}/ts/${event}/${n}.in`;
 
   const inputExists = await exists(filePath);
 
@@ -75,7 +140,7 @@ if (!aocFolder || args.folder) {
   }
 }
 
-const event = args.y ?? 2022;
+const event: number = args.y ?? 2022;
 const day = args._.pop() as number | undefined;
 
 if (args.i && aocFolder && session && day) {
@@ -97,4 +162,16 @@ if (args.i && aocFolder && session && day) {
       console.log(chalk.red(`Error when running '${day}.ts'`));
     }
   }
+}
+
+const Leaderboards: Record<string, number> = {
+  spotify: 318415,
+  tobii: 371692,
+  viaplay: 626539,
+};
+
+const leadName: string = args.l;
+
+if (leadName && session && event) {
+  await getLeaderboardInfo(event, Leaderboards[leadName] ?? Leaderboards.spotify);
 }
